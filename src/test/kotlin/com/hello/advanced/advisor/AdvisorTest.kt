1354e030f52a5cb8833e2c9d853186026242bb09
won0935@gmail.com <won0935@gmail.com>
2023-02-27 20:37:20 +0900

    [ADD] MyPointcut 생성

--- src/test/kotlin/com/hello/advanced/advisor/AdvisorTest.kt
+++ src/test/kotlin/com/hello/advanced/advisor/AdvisorTest.kt
@@ -3,15 +3,19 @@ package com.hello.advanced.advisor
 import com.hello.advanced.common.advice.TimeAdvice
 import com.hello.advanced.common.service.ServiceImpl
 import com.hello.advanced.common.service.ServiceInterface
+import mu.KotlinLogging
 import org.junit.jupiter.api.Test
+import org.springframework.aop.ClassFilter
+import org.springframework.aop.MethodMatcher
 import org.springframework.aop.Pointcut
 import org.springframework.aop.framework.ProxyFactory
 import org.springframework.aop.support.DefaultPointcutAdvisor
+import java.lang.reflect.Method
 
 class AdvisorTest {
 
     @Test
-    fun advisorTest() {
+    fun advisorTest1() {
 
         val target = ServiceImpl()
         val proxyFactory = ProxyFactory(target)
@@ -24,5 +28,53 @@ class AdvisorTest {
         proxy.find()
 
     }
+
+
+    @Test
+    fun advisorTest2() {
+        val target = ServiceImpl()
+        val proxyFactory = ProxyFactory(target)
+        val advisor = DefaultPointcutAdvisor(MyPointcut(), TimeAdvice())
+        proxyFactory.addAdvisor(advisor)
+
+        val proxy = proxyFactory.proxy as ServiceInterface
+
+        proxy.save()
+        proxy.find()
+
+    }
+
+    class MyPointcut : Pointcut {
+        override fun getClassFilter(): ClassFilter {
+            return ClassFilter.TRUE
+        }
+
+        override fun getMethodMatcher(): MethodMatcher {
+            return MyMethodMatcher()
+        }
+    }
+
+    class MyMethodMatcher : MethodMatcher {
+
+        private val matchName = "save"
+        private val log = KotlinLogging.logger {}
+
+        override fun matches(method: Method, targetClass: Class<*>): Boolean {
+            val result = method.name == matchName
+            log.info { "포인트컷 호출 $method $targetClass" }
+            log.info { "포인트컷 결과 $result" }
+            return result
+        }
+
+        override fun matches(method: Method, targetClass: Class<*>, vararg args: Any?): Boolean {
+            return false
+        }
+
+        override fun isRuntime(): Boolean {
+            return false
+        }
+
+    }
+
 
 }
